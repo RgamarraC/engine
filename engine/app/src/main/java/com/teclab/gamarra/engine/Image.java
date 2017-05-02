222d7fb74a4a45f5df9fec9cd854a2ec40c3fdb3
Alumno <Alumno>
2017-05-02 13:51:41 -0500

    imgas2/5

--- engine/app/src/main/java/com/teclab/gamarra/engine/Image.java
+++ engine/app/src/main/java/com/teclab/gamarra/engine/Image.java
@@ -3,32 +3,47 @@ package com.teclab.gamarra.engine;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 
 
 public class Image extends DisplayObject {
 String key;
 Resources resources;
-
+Bitmap a;
   public Image(String _key, String _tag,int source, Resources _resources) {
             this.resources=_resources;
             this.key =_key;
              _resources.loadTexture(_key,source);
-           Bitmap aux = resources.getTexture(key);
-              this.width=aux.getWidth();
-              this.height=aux.getHeight();
+           a = resources.getTexture(key);
+              this.width=a.getWidth();
+              this.height=a.getHeight();
 
-       }
+      }
             public void onDraw() {
                 Paint p = new Paint();
-                float n =x-this.width*0.5F;
-                float m =y-this.height*0.5F;
+                p.setAlpha((int) (opacity*255));
+                float n;
+                float m;
                 Canvas canvas = resources.canvas;
                 canvas.rotate(rotation);
-                canvas.drawBitmap(resources.getTexture(key),new Rect(0,0,this.width,this.height),new RectF(n,m,this.width*this.scaleX,this.height*this.scaleY),p);
+                if(scaleY<0||scaleX<0){
+                    Matrix matrix = new Matrix();
+                    matrix.preScale(scaleX,scaleY);
+                    a = Bitmap.createBitmap(resources.getTexture(key),0,0,this.width,this.height,matrix,false);
+                    n =x-a.getWidth()*0.5F;
+                    m =y-a.getHeight()*0.5F;
+                    canvas.drawBitmap(a,n,m,p);
+                }
+                else {
+                    n =x-this.width*0.5F;
+                    m =y-this.height*0.5F;
+                    canvas.drawBitmap(a, new Rect(0, 0, this.width, this.height), new RectF(n, m, this.width * this.scaleX, this.height * this.scaleY), p);
+                }
                 canvas.rotate(-rotation);
 
 
--- engine/app/src/main/java/com/teclab/gamarra/engine/MainScene.java
+++ engine/app/src/main/java/com/teclab/gamarra/engine/MainScene.java
@@ -9,18 +9,15 @@ public class MainScene extends DisplayContainer{
 
 
     Resources res;
-    Image img, img2;
+    Image img;
     public MainScene(Resources res){
         super();
         this.res = res;
         img = new Image("bad","bad",R.drawable.bad1,res);
-        img2 = new Image("blood","blood",R.drawable.blood1,res);
-img2.scaleX=20;
-        img2.scaleY=5;
-                img.x=150;
-        img.y=6;
-        img2.rotation = 45;
-        this.addChild(img2);
+        img.scaleX=-2;img.x = 0;
+
+
+
         this.addChild(img);
 
 
